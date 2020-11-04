8cfaeda4a0513aadce47397ba74b78b066cca4e7
Yoojin Kim <yoojinkim@Yoojinui-MacBookPro.local>
2020-11-04 21:47:56 +0900

    folder setting

---
+++ src/components/Text/index.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import * as colors from "../../data/constants";
+import "../../App.css";
+
+const Text = ({ children, bold, size, color, style, mode, className }) => {
+  const textStyle = {
+    whiteSpace: "normal",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    width: "100%",
+    height: "100%",
+    textAlign: "left",
+    wordWrap: "break-word",
+  };
+
+  const getColor = () => {
+    switch (color) {
+      case "white":
+        return colors.defaultWhite;
+      case "black":
+        return colors.defaultBlack;
+      case "red":
+        return colors.red;
+      case "primary":
+        return colors.primary;
+      default:
+        return color;
+    }
+  };
+
+  const getStyle = () => {
+    switch (mode) {
+      case "ellipsis":
+        return {
+          ...textStyle,
+          color: getColor(),
+          ...style,
+        };
+      default:
+        return {
+          color: getColor(),
+          ...style,
+        };
+    }
+  };
+
+  const renderTitle = () => {
+    switch (typeof children) {
+      case "string":
+        let title = children.toString().split("\n");
+        return title.map((v, index) => {
+          return (
+            <div
+              key={index}
+              style={getStyle()}
+              className={`${bold == true && "fbold"} fs${size}`}
+            >
+              {v}
+            </div>
+          );
+        });
+      default:
+        return (
+          <div
+            style={getStyle()}
+            className={`${bold == true && "fbold"} fs${size}`}
+          >
+            {children}
+          </div>
+        );
+    }
+  };
+
+  return (
+    <div
+      style={getStyle()}
+      className={`${className} ${bold == true && "fbold"} fs${size}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+Text.defaultProps = {
+  bold: false,
+  size: 17,
+  color: "#000",
+  style: null,
+  mode: "normal",
+};
+
+export default Text;
---
+++ src/pages/Landing Page/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../App.css";
+import Text from "../../components/Text";
+
+const LandingPage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text size={36} bold>
+        Test
+      </Text>
+      <Text size={36} bold>
+        Test
+      </Text>
+    </div>
+  );
+};
+
+export default LandingPage;
