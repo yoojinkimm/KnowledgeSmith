807877d62d3b0fa2a2154f88a824fb4c6de2aea5
Yoojin Kim <yoojinkim@Yoojinui-MacBookPro.local>
2020-11-14 13:27:25 +0900

    Updates

---
+++ precache-manifest.f2bfd498030d79fca8b62a6a8608c9c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e0e21e1523ea49641765e8dc89ce26",
+    "url": "/wiki-project/index.html"
+  },
+  {
+    "revision": "4910e43afc1047cfec73",
+    "url": "/wiki-project/static/css/main.16b6200d.chunk.css"
+  },
+  {
+    "revision": "f648cacd2d417088ff95",
+    "url": "/wiki-project/static/js/2.4c538136.chunk.js"
+  },
+  {
+    "revision": "bf71ac297fcfe9a3d2d199a48fc3f60c",
+    "url": "/wiki-project/static/js/2.4c538136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4910e43afc1047cfec73",
+    "url": "/wiki-project/static/js/main.3c55f2bf.chunk.js"
+  },
+  {
+    "revision": "2ccd20c02c43cba770ba",
+    "url": "/wiki-project/static/js/runtime-main.45e2a7db.js"
+  }
+]);
